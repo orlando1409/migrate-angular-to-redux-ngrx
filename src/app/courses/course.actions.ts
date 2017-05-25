34bdccb3b088901a3a79de10179dfa2a906eef48
orlando1409 <orlando1409@hotmail.com>
2017-05-24 20:22:18 -0500

    function 8

--- src/app/courses/course.actions.ts
+++ src/app/courses/course.actions.ts
@@ -44,4 +44,8 @@ export class CourseActions {
     randomNumber3(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min
     };
+
+    randomNumber7(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min
+    };
 };
